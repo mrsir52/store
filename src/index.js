b207a82b74e95f250a5d0743b96e3ef88de1b882
Manny <coachmannytw@gmail.com>
2019-03-30 21:19:46 -0600

    test store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Link, BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Admin from "./components/Admin";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <Router>
+    <Fragment>
+      <nav className="nav justify-content-center">
+        <Link className="nav-link" to="/" title="Home">
+          Home
+        </Link>
+        <Link
+          className="nav-link"
+          to="/Admin"
+          title="Admin"
+        >
+          Admin
+        </Link>
+      </nav>
+      <div className="nav-link">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Admin" component={Admin} />
+        </Switch>
+      </div>
+    </Fragment>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
